9e7d4b3072dcb42d6623f680b1706cdb62ea8c9e
Thiago C Silva <tkreserva@gmail.com>
2024-06-24 01:44:08 -0300

    mudanças de teste 15

---
+++ salas/apps.py
@@ -0,0 +1,4 @@
+from django.apps import AppConfig
+class SalasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'salas'
---
+++ setup/urls.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from django.urls import path
+from salas.views import inicio
+urlpatterns = [path('admin/', admin.site.urls),path('',inicio)] 
